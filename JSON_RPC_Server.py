c7e6aade387711524c6120eebd77a4f286e45a17
sash00k <bochkarev.sash00k@gmail.com>
2024-05-03 13:09:55 +0700

    A bit of refactoring

--- JSON_RPC_Server.py
+++ JSON_RPC_Server.py
@@ -20,6 +20,7 @@ from PydanticModels import *
 app = jsonrpc.API()
 api_v1 = jsonrpc.Entrypoint('/api/v1/jsonrpc')
 
+
 async def remove_files_and_folders():
     items_to_remove = [
         'iteration_statistic.txt',
@@ -43,20 +44,15 @@ async def remove_files_and_folders():
         else:
             pass
 
-
 async def process_files_in_folder(folder: str, model_class: type) -> Dict[str, List[BaseModelWithFileParsing]]:
     data = {}
-
     try:
         files = sorted(os.listdir(folder), key=lambda x: int(re.search(r'\d+', x).group()))
-
         for file_number, filename in enumerate(files, start=1):
             if filename.startswith(f'{model_class.__name__}.'):
                 file_path = os.path.join(folder, filename)
                 data[f'{model_class.__name__}.{file_number}'] = model_class.parse_file(file_path)
-
         return data
-
     except Exception as error:
         raise Error(
             data={'details': f"JSON-RPC server error while reading '{folder}' folder:\n{error}", 'status_code': 502})
@@ -65,30 +61,21 @@ async def process_files_in_folder(folder: str, model_class: type) -> Dict[str, L
 async def run_fc_2022initstrss():
     await remove_files_and_folders()
     subprocess.run(['FC_2022initStrss.exe'], check=True)
-    import time
 
 
 async def check_file(fileName, status_code):
     if not os.path.exists(fileName):
         raise Error(data={'details': f"The file '{fileName}' was not found", 'status_code': status_code})
-
     return True
 
 async def run_pioner_exe() -> ProcessOutputModel:
     if await check_file('input.txt', 500):
         try:
             await run_fc_2022initstrss()
-
             displacements_data = await process_files_in_folder('Displacements', Displacements)
             stress_data = await process_files_in_folder('Stress', Stress)
-
-            result = ProcessOutputModel(
-                displacements=displacements_data,
-                stress=stress_data
-            )
-
+            result = ProcessOutputModel(displacements=displacements_data,stress=stress_data)
             return result
-
         except subprocess.CalledProcessError as e:
             raise Error(data={'details': f'Error while executing the process: {e}', 'status_code': 501})
         except Exception as e:
@@ -98,7 +85,6 @@ async def run_selected_template(in_file: InputTemplateModel) -> ProcessOutputMod
     content = in_file.fileContent
     with open('input.txt', 'w') as output_file:
         output_file.write(content)
-
     return await run_pioner_exe()
 
 def get_template_files() -> List[str]:
@@ -117,10 +103,7 @@ async def save_input_template(in_params: InputTemplateModel) -> bool:
         ret['result'] = True
     except Exception as e:
         raise Error(data={'details': f'JSON-RPC server error: {e}', 'status_code': 508})
-        return False
-
     return True
-
 
 @api_v1.method(errors=[Error])
 async def get_template_list() -> List[InputTemplateModel]:
@@ -128,7 +111,6 @@ async def get_template_list() -> List[InputTemplateModel]:
     files = get_template_files()
     for it in files:
         ret.append(InputTemplateModel(fileName=it))
-
     return ret
 
 @api_v1.method(errors=[Error])
@@ -139,14 +121,12 @@ async def run_pioner(in_params: InputTemplateModel) -> ProcessOutputModel:
 
 @api_v1.method(errors=[Error])
 async def get_content_by_name(in_params: InputTemplateModel) -> InputTemplateModel:
-
     if await check_file(f'InputTemplates/{in_params.fileName}', 401):
         try:
             with open(f'InputTemplates/{in_params.fileName}', 'r') as file:
                 content = file.read()
         except Exception as e:
             raise Error(data={'details': f'JSON-RPC get_content_by_name error: {e}', 'status_code': 509})
-
     return InputTemplateModel(fileName=in_params.fileName, fileContent=content)
 
 
