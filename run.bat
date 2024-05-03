e9c109d843620014c74637a384532b7effd9632b
sash00k <bochkarev.sash00k@gmail.com>
2024-05-03 14:25:21 +0700

    script update

--- run.bat
+++ run.bat
@@ -4,6 +4,12 @@ echo Сложили локальные изменения.
 git fetch
 git pull
 echo Стянули обновления.
+
+IF NOT EXIST .env (
+    echo HOST_IP='127.0.0.1' > .env
+    echo Создали .env файл.
+)
+
 start python JSON_RPC_Server.py
 echo Запустили JSON_RPC_Server.py
 start python BFF_FASTAPI_Server.py
